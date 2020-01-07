40fc14c37d8d682af54e50c8b784796fbcfac4b7
Brady Aiello <bba@amazon.com>
2020-01-07 14:24:49 -0800

    Removed AsyncTask method from NumberFactsService.
    Signed-off-by: Brady Aiello <bba@amazon.com>

--- app/src/main/java/com/bradyaiello/asynchrony/services.kt
+++ app/src/main/java/com/bradyaiello/asynchrony/services.kt
@@ -2,22 +2,20 @@ package com.bradyaiello.asynchrony
 
 import io.reactivex.Single
 import retrofit2.Call
-import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
 
-// Good
+
 interface NumberFactsService {
-
-    @GET("{number}")
-    fun getNumberFactAsyncTask(@Path (value = "number") number: String) : Response<String>
-
+    // Good
     @GET("{number}")
     fun getNumberFactCall(@Path (value = "number") number: String) : Call<String>
 
+    // Better
     @GET("{number}")
     fun getNumberFactSingle(@Path (value = "number") number: String) : Single<String>
 
+    // Best
     @GET("{number}")
     suspend fun getNumberFactString(@Path (value = "number") number: String) : String
 }
